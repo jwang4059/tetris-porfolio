c50fffae300d34669b8abef58244e67f6dd60c01
jwang0110 <johnw7@uci.edu>
2023-02-27 23:59:37 -0800

    experience secion update

--- src/components/Content/Experience/Experience.tsx
+++ src/components/Content/Experience/Experience.tsx
@@ -1,12 +1,33 @@
 import React from "react";
+import Image from "next/image";
+import { ExperienceType, experiencesData } from "@/utils/data";
+import styles from "./Experience.module.scss";
 
 type Props = {};
 
+const Card = ({ data }: { data: ExperienceType }) => (
+	<div className={styles["card"]}>
+		<div className={styles["card__image__wrapper"]}>
+			<Image src={data.image.src} alt={data.image.alt} />
+		</div>
+		<div className={styles["card__header"]}>
+			<h3>{data.company}</h3>
+			<span>{data.position}</span>
+			<span>{data.date}</span>
+		</div>
+		<div className={styles["card__content"]}>
+			<p>{data.description}</p>
+		</div>
+	</div>
+);
+
 const Experience = (props: Props) => {
 	return (
-		<section className="my-experience" id="experience">
+		<section className={styles["experience"]} id="experience">
 			<h2 className="section__title">My Experience</h2>
-			<p className="section__subtitle"></p>
+			{experiencesData.map((x) => (
+				<Card key={x.company} data={x} />
+			))}
 		</section>
 	);
 };
