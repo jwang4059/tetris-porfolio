b9c09daa2ea77675ad29e813088bab634955347c
jwang0110 <johnw7@uci.edu>
2023-02-28 23:55:35 -0800

    update experience styling

--- src/components/Content/Experience/Experience.tsx
+++ src/components/Content/Experience/Experience.tsx
@@ -8,12 +8,12 @@ type Props = {};
 const Card = ({ data }: { data: ExperienceType }) => (
 	<div className={styles["card"]}>
 		<div className={styles["card__image__wrapper"]}>
-			<Image src={data.image.src} alt={data.image.alt} />
+			<Image src={data.image.src} alt={data.image.alt} fill />
 		</div>
 		<div className={styles["card__header"]}>
-			<h3>{data.company}</h3>
-			<span>{data.position}</span>
-			<span>{data.date}</span>
+			<h3 className={styles["card__company"]}>{data.company}</h3>
+			<span className={styles["card__position"]}>{data.position}</span>
+			<span className={styles["card__date"]}>{data.date}</span>
 		</div>
 		<div className={styles["card__content"]}>
 			<p>{data.description}</p>
