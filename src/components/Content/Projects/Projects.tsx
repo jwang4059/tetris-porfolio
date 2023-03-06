f8e371b79f9c1571d98470b9daf63e2508ea15ae
jwang0110 <johnw7@uci.edu>
2023-03-05 23:54:30 -0800

    add stack navigation button

--- src/components/Content/Projects/Projects.tsx
+++ src/components/Content/Projects/Projects.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
 import Tetris from "@/components/Tetris/Tetris/Tetris";
 import { ProjectType, projectsData, iconMap } from "@/utils/data";
 import styles from "./Projects.module.scss";
 import { IconType } from "react-icons/lib";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 type Props = {};
 
@@ -17,6 +18,36 @@ const StackIcon = ({ name, Icon }: { name: string; Icon: IconType }) => (
 );
 
 const Project = ({ data }: { data: ProjectType }) => {
+	const iconListRef = useRef<HTMLUListElement>(null);
+
+	const showLeft = () => {
+		if (!iconListRef.current) return false;
+
+		if (iconListRef.current.scrollLeft !== 0) return true;
+		else return false;
+	};
+
+	const showRight = () => {
+		if (!iconListRef.current) return false;
+
+		if (
+			iconListRef.current.scrollLeft !==
+			iconListRef.current.scrollWidth - iconListRef.current.clientWidth
+		) {
+			console.log({
+				left: iconListRef.current.scrollLeft,
+				leftMax:
+					iconListRef.current.scrollWidth - iconListRef.current.clientWidth,
+			});
+			return true;
+		} else return false;
+	};
+
+	const scroll = (offset: number) => {
+		console.log(iconListRef.current);
+		if (iconListRef.current) iconListRef.current.scrollLeft += offset;
+	};
+
 	return (
 		<section className={styles["project"]}>
 			<div className={styles["project__image__wrapper"]}>
@@ -39,17 +70,38 @@ const Project = ({ data }: { data: ProjectType }) => {
 					Source Code
 				</a>
 				<p className={styles["project__description"]}>{data.description}</p>
-				<ul className={styles["project__icons__list"]}>
-					{data.stack &&
-						data.stack.map((x) => (
-							<StackIcon
-								key={x}
-								name={iconMap[x].name}
-								Icon={iconMap[x].icon}
-							/>
-						))}
-				</ul>
-				{data?.url ? (
+				{data.stack && (
+					<div className={styles["project__stack"]}>
+						<div className={styles["project__stack__navigation"]}>
+							{showLeft() && (
+								<span
+									className={styles["project__stack__navigation__arrow"]}
+									onClick={() => scroll(-10)}
+								>
+									<MdChevronLeft />
+								</span>
+							)}
+							{showRight() && (
+								<span
+									className={styles["project__stack__navigation__arrow"]}
+									onClick={() => scroll(10)}
+								>
+									<MdChevronRight />
+								</span>
+							)}
+						</div>
+						<ul ref={iconListRef} className={styles["project__icons__list"]}>
+							{data.stack.map((x) => (
+								<StackIcon
+									key={x}
+									name={iconMap[x].name}
+									Icon={iconMap[x].icon}
+								/>
+							))}
+						</ul>
+					</div>
+				)}
+				{data.url && (
 					<a
 						className={styles["project__link"]}
 						href={data.url}
@@ -58,7 +110,7 @@ const Project = ({ data }: { data: ProjectType }) => {
 					>
 						See Live
 					</a>
-				) : null}
+				)}
 			</div>
 		</section>
 	);
